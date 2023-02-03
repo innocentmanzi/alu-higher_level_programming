46583f3227b0e2c7427e2ee8701fb91918ec3b2c
innocentmanzi <i.manzi@alustudent.com>
2023-02-03 05:19:07 -0800

    task 7

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"
+word = "School"
 word_first_3 = word[:3]
-word_last_2 = word[7:]
-middle_word = word[1:8]
+word_last_2 = word[4:]
+middle_word = word[1:5]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
