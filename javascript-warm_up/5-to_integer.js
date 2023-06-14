8b1e2f64e86a23dfb6063dcdc50ffe74768469b8
innocentmanzi <i.manzi@alustudent.com>
2023-06-14 04:26:41 -0700

    task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
 
-if (num) {
-  console.log('My number: ' + num);
+if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
 }
